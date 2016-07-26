5cf20d471ded3af39f07bf05a52885fc2d172e54
lidaming <1099057173@qq.com>
2016-07-26 17:20:58 +0800

    第二次提交。7.26引导界面的动画和颜色渐变

--- MyGitDroid/app/src/main/java/feicui/mygitdroid/splash/SplashPagerFragment.java
+++ MyGitDroid/app/src/main/java/feicui/mygitdroid/splash/SplashPagerFragment.java
@@ -1,5 +1,6 @@
 package feicui.mygitdroid.splash;
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import butterknife.Bind;
+import butterknife.BindColor;
 import butterknife.ButterKnife;
 import feicui.mygitdroid.R;
 import me.relex.circleindicator.CircleIndicator;
@@ -23,14 +25,22 @@ public class SplashPagerFragment extends Fragment {
     @Bind(R.id.viewPager)
     ViewPager viewPager;
     @Bind(R.id.indicator)
-    CircleIndicator indicator;
-    ImageView ivPhoneBlank;
-    ImageView ivPhoneFont;
-    FrameLayout layoutPhone;
+    CircleIndicator indicator;//指示器
+
     @Bind(R.id.content)
     FrameLayout content;
+    @Bind(R.id.ivPhoneBlank)
+    ImageView ivPhoneBlank;
+    @Bind(R.id.ivPhoneFont)
+    ImageView ivPhoneFont;
+    @Bind(R.id.layoutPhoneInner)
+    FrameLayout layoutPhoneInner;
+    @Bind(R.id.layoutPhone)
+    FrameLayout layoutPhone;
     private SplashPagerAdapter adapter;
-
+    @BindColor(R.color.colorRed) int colorRed;
+    @BindColor(R.color.colorYellow) int colorYellow;
+    @BindColor(R.color.colorGreen) int colorGreen;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,11 +52,12 @@ public class SplashPagerFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
-//        indicator = (CircleIndicator) view.findViewById(R.id.indicator);
         adapter = new SplashPagerAdapter(getContext());
         viewPager.setAdapter(adapter);
         indicator.setViewPager(viewPager);
+//        colorGreen=getResources().getColor(R.color.colorGreen);
+//        colorRed=getResources().getColor(R.color.colorRed);
+//        colorYellow=getResources().getColor(R.color.colorYellow);
         // 添加ViewPager监听(为了动画)
         viewPager.addOnPageChangeListener(pageColorListener);
         viewPager.addOnPageChangeListener(phoneViewListener);
@@ -54,8 +65,18 @@ public class SplashPagerFragment extends Fragment {
 
     // 主要为了做背景颜色渐变处理
     private ViewPager.OnPageChangeListener pageColorListener = new ViewPager.OnPageChangeListener() {
+        final ArgbEvaluator argbEvaluator=new ArgbEvaluator();
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        if (position==0){
+            int color= (int) argbEvaluator.evaluate(positionOffset,colorGreen,colorRed);
+            content.setBackgroundColor(color);
+            return;
+        }
+            if (position==1){
+                int color= (int) argbEvaluator.evaluate(positionOffset,colorRed,colorYellow);
+                content.setBackgroundColor(color);
+            }
 
         }
 
@@ -74,7 +95,19 @@ public class SplashPagerFragment extends Fragment {
     private ViewPager.OnPageChangeListener phoneViewListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+         if (position==0){//第一个页面到第二个页面的手机放缩
+             float scale=0.3f+positionOffset*0.7f;
+             layoutPhone.setScaleX(scale);
+             layoutPhone.setScaleY(scale);
 
+             int scroll = (int) ((positionOffset-1)*300);
+             layoutPhone.setTranslationX(scroll);
+          ivPhoneFont.setAlpha(positionOffset);
+             return;
+         }
+            if (position==1){
+                layoutPhone.setTranslationX(-positionOffsetPixels);
+            }
         }
 
         @Override
